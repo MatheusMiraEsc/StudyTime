33a8dba7ccb1e7c9593832bb6d0241cf8c315f05
MatheusMiraEsc <mme@cesar.school>
2025-07-31 14:56:06 -0300

    Iniciando o banco de dados mysql

--- projeto_pomodoro/projeto_pomodoro/settings.py
+++ projeto_pomodoro/projeto_pomodoro/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
+pymysql.install_as_MySQLdb()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'projeto_pomodoro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'studytimedb',            
+        'USER': 'user_django',           
+        'PASSWORD': 'mysql',   
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
